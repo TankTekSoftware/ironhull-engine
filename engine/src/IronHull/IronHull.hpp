e84034af5ed07fbe867cad27d4379b919252ef6e
msmith-codes <msmith.softwaredev@gmail.com>
2026-09-01 13:49:11 -0400

    Removed Deprecated Nodes

--- engine/src/IronHull/IronHull.hpp
+++ engine/src/IronHull/IronHull.hpp
@@ -4,11 +4,15 @@
 
 #include <IronHull/input/Input.hpp>
 
+#include <IronHull/nodes/CharacterBody2D.hpp>
+#include <IronHull/nodes/CollisionShape2D.hpp>
+#include <IronHull/nodes/PhysicsBody2D.hpp>
+#include <IronHull/nodes/PhysicsWorld2D.hpp>
+
 #include <IronHull/render/RenderPass.hpp>
 
 #include <IronHull/scene/Scene.hpp>
 #include <IronHull/scene/SceneManager.hpp>
-#include <IronHull/scene/Layer.hpp>
 
 #include <IronHull/utils/ColorUtils.hpp>
 #include <IronHull/utils/DataUtils.hpp>
